e2a4c746843c2004bdc1332c62687384da1d768d
Billy Messenger <BillyDM@tutamail.com>
2021-07-31 15:21:21 -0500

    work on sampling engine

--- src/backend/timeline/sampler.rs
+++ src/backend/timeline/sampler.rs
@@ -6,6 +6,8 @@ use crate::backend::MAX_BLOCKSIZE;
 
 // If the pcm sample rate matches the target sample rate, then no resampling will occur and samples will
 // just be copied (with the start being rounded to the nearest sample).
+//
+// This will overwrite the samples in the given `out` buffer.
 pub fn sample_stereo(
     frames: usize,
     sample_rate: SampleRate,
@@ -30,34 +32,11 @@ pub fn sample_stereo(
         .to_seconds(sample_rate)
         .to_sub_sample(pcm.sample_rate());
 
-    let mut zero_smps_before = 0;
-    let mut zero_smps_after = 0;
     let mut pcm_frames = pcm_frames.0 as usize;
 
-    let pcm_start_smp = if pcm_start_smp.0 < 0 {
-        // Skip until the first sample in the pcm resource.
-        zero_smps_before = (0 - pcm_start_smp.0) as usize;
-        if zero_smps_before > pcm_frames {
-            // Out of range. Nothing to do.
-            return;
-        }
-
-        pcm_frames -= zero_smps_before;
-
-        0
-    } else {
-        pcm_start_smp.0 as usize
-    };
-
-    if pcm_start_smp >= pcm.len() {
+    if pcm_start_smp.0 >= pcm.len() as i64 {
         // Out of range. Nothing to do.
         return;
-    }
-
-    if pcm_start_smp + pcm_frames >= pcm.len() {
-        // Skip all samples after the last sample in the pcm resource.
-        zero_smps_after = (pcm_start_smp + pcm_frames) - pcm.len();
-        pcm_frames -= zero_smps_after;
     }
 
     let pcm_range_info = PcmRangeInfo {
@@ -65,13 +44,11 @@ pub fn sample_stereo(
         pcm_start_sub_smp,
         pcm_frames,
         pcm_frames_sub_smp,
-        zero_smps_before,
-        zero_smps_after,
     };
 
     if pcm.sample_rate() == sample_rate {
         // No need to resample, just copy.
-        copy_samples(pcm, &pcm_range_info, out, out_offset);
+        copy_samples_nearest(pcm, &pcm_range_info, out, out_offset);
     } else {
         // Resample to project sample rate.
 
@@ -80,63 +57,70 @@ pub fn sample_stereo(
 }
 
 struct PcmRangeInfo {
-    /// Where to start reading from in the pcm resource.
-    pcm_start_smp: usize,
+    /// Where to start reading from in the pcm resource (floored).
+    pcm_start_smp: SampleTime,
 
     /// Additional sub-sample amount to `pcm_start_smp` (from `[0.0, 1.0)`).
     pcm_start_sub_smp: f64,
 
-    /// How many frames to read from the pcm resource.
+    /// How many frames to read from the pcm resource (floored).
     pcm_frames: usize,
 
     /// Additional sub-sample amount to `pcm_frames` (from `[0.0, 1.0)`).
     pcm_frames_sub_smp: f64,
-
-    /// How many zero samples to add before reading the pcm samples.
-    zero_smps_before: usize,
-
-    /// How many zero samples to add after reading the pcm samples.
-    zero_smps_after: usize,
 }
 
-fn copy_samples(
+fn copy_samples_nearest(
     pcm: &StereoPcm,
     pcm_range_info: &PcmRangeInfo,
     out: &mut StereoAudioBlockBuffer,
     mut out_offset: usize,
 ) {
-    let (pcm_start, pcm_frames, zero_smps_before) = if pcm_range_info.pcm_start_sub_smp >= 0.5 {
+    let pcm_start_smp = if pcm_range_info.pcm_start_sub_smp >= 0.5 {
         // Round up to next sample.
-        let pcm_start = pcm_range_info.pcm_start_smp + 1;
-
-        // Check that we are still in bounds.
-        let pcm_frames = if pcm_start + pcm_range_info.pcm_frames >= pcm.len() {
-            pcm.len() - pcm_start
-        } else {
-            pcm_range_info.pcm_frames
-        };
-
-        let zero_smps_before = if pcm_range_info.zero_smps_before > 0 {
-            pcm_range_info.zero_smps_before - 1
-        } else {
-            pcm_range_info.zero_smps_before
-        };
-
-        (pcm_start, pcm_frames, zero_smps_before)
+        pcm_range_info.pcm_start_smp + SampleTime(1)
     } else {
-        (
-            pcm_range_info.pcm_start_smp,
-            pcm_range_info.pcm_frames,
-            pcm_range_info.zero_smps_before,
-        )
+        pcm_range_info.pcm_start_smp
     };
 
-    out_offset += zero_smps_before;
+    let (zeros_after, mut pcm_frames) = if pcm_start_smp.0 + pcm_range_info.pcm_frames as i64
+        > pcm.len() as i64
+    {
+        let zeros_after = (pcm_start_smp.0 + pcm_range_info.pcm_frames as i64) as usize - pcm.len();
+        let pcm_frames = pcm_range_info.pcm_frames - zeros_after;
+        (zeros_after, pcm_frames)
+    } else {
+        (0, pcm_range_info.pcm_frames)
+    };
+
+    let (pcm_start, zeros_before) = if pcm_start_smp.0 < 0 {
+        (0, (0 - pcm_start_smp.0) as usize)
+    } else {
+        (pcm_start_smp.0 as usize, 0)
+    };
+
+    pcm_frames -= zeros_before;
+
+    if zeros_before > 0 {
+        // Fill in zeros.
+        &mut out.left[out_offset..out_offset + zeros_before].fill(0.0);
+        &mut out.right[out_offset..out_offset + zeros_before].fill(0.0);
+    }
+
+    out_offset += zeros_before;
 
     if pcm_frames > 0 {
         &mut out.left[out_offset..out_offset + pcm_frames]
             .copy_from_slice(&pcm.left()[pcm_start..pcm_start + pcm_frames]);
         &mut out.right[out_offset..out_offset + pcm_frames]
             .copy_from_slice(&pcm.right()[pcm_start..pcm_start + pcm_frames]);
+    }
+
+    out_offset += pcm_frames;
+
+    if zeros_after > 0 {
+        // Fill in zeros.
+        &mut out.left[out_offset..out_offset + zeros_after].fill(0.0);
+        &mut out.right[out_offset..out_offset + zeros_after].fill(0.0);
     }
 }
