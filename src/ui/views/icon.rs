c60bd9d658c02b3fcb481a4cfb2cefafbe169b3f
Lunae Somnia <lunaesomnia@protonmail.com>
2022-04-03 17:08:32 +0200

    Small optimizations

--- src/ui/views/icon.rs
+++ src/ui/views/icon.rs
@@ -1,43 +1,25 @@
 use vizia::*;
 
-use crate::ui::icons::IconType;
-
-#[derive(Lens)]
-pub struct IconData {}
-
-impl Model for IconData {
-    fn event(&mut self, cx: &mut Context, event: &mut Event) {}
-}
+use crate::ui::icons::IconCode;
 
 pub struct Icon {}
 
 impl Icon {
-    pub fn new<'a>(cx: &'a mut Context, icon: IconType) -> Handle<'a, Self> {
+    pub fn new<'a>(cx: &'a mut Context, icon: IconCode, size: f32) -> Handle<'a, Self> {
         Self {}.build2(cx, |cx| {
-
-            let size = 24.0 * 4.0;
             let icon_str: &str = icon.into();
 
-            IconData {}.build(cx);
-
-            HStack::new(cx, |cx| {
-                Label::new(cx, icon_str)
-                    .width(Pixels(size * 0.666))
-                    .height(Pixels(size * 0.666))
-                    .font_size(size * 0.666)
-                    .font("meadowlark")
-                    .position_type(PositionType::SelfDirected)
-                    .z_order(15)
-                    .class("icon-svg");
-            })
-            .width(Pixels(size))
-            .height(Pixels(size))
-            .class("icon");
+            Label::new(cx, icon_str)
+                .width(Pixels(size))
+                .height(Pixels(size))
+                .font_size(size * 0.666)
+                .font("meadowlark")
+                .position_type(PositionType::SelfDirected)
+                .z_order(15)
+                .class("icon");
         })
     }
 }
-
-pub enum IconEvent {}
 
 impl View for Icon {
     fn event(&mut self, cx: &mut Context, event: &mut Event) {}
