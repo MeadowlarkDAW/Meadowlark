8a82f892c6a5e22b4d32b0f24dff522f42efb0df
Lunae Somnia <lunaesomnia@protonmail.com>
2022-04-04 12:22:25 +0200

    Update icon.rs

--- src/ui/views/icon.rs
+++ src/ui/views/icon.rs
@@ -5,17 +5,28 @@ use crate::ui::icons::IconCode;
 pub struct Icon {}
 
 impl Icon {
-    pub fn new<'a>(cx: &'a mut Context, icon: IconCode, size: f32) -> Handle<'a, Self> {
+    // Creates an Icon with a set size for the outer frame and the icon.
+    pub fn new<'a>(cx: &'a mut Context, icon: IconCode, frame_size: f32, icon_size: f32) -> Handle<'a, Self> {
         Self {}.build2(cx, |cx| {
             let icon_str: &str = icon.into();
 
+            // Icon can't be bigger than the frame it's held in.
+            if icon_size > frame_size {
+                icon_size = frame_size;
+            }
+
             Label::new(cx, icon_str)
-                .width(Pixels(size))
-                .height(Pixels(size))
-                .font_size(size * 0.666)
+                .width(Pixels(frame_size))
+                .height(Pixels(frame_size))
+                .font_size(icon_size)
                 .font("meadowlark")
                 .class("icon");
         })
+    }
+
+    // Creates an Icon with a frame of `24px` and an icon of `16px`.
+    pub fn default<'a>(cx: &'a mut Context, icon: IconCode) -> Handle<'a, Self> {
+        Self::new(cx, icon, 24.0, 16.0)
     }
 }
 
