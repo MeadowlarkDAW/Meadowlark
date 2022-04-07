a1b02ffdd7d285f8ce937168c9ca7059ba753da8
RitzGit <marvin@wallenfang.de>
2022-04-07 17:38:00 +0200

    Simple implementation of top right elements

--- src/ui/panels/top_bar.rs
+++ src/ui/panels/top_bar.rs
@@ -13,13 +13,94 @@ use crate::ui::{icons::IconCode, Icon, PanelEvent};
 
 pub fn top_bar(cx: &mut Context) {
     HStack::new(cx, |cx| {
-        Button::new(
-            cx,
-            |cx| cx.emit(PanelEvent::TogglePianoRoll),
-            |cx| Icon::new(cx, IconCode::Piano, 24.0, 16.0),
-        )
-        .left(Stretch(1.0))
-        .right(Pixels(20.0));
+        Label::new(cx, "Menu Button");
+        Label::new(cx, "Play Controls");
+        HStack::new(cx, |cx| {
+            VStack::new(cx, |cx| {
+                HStack::new(cx, |cx| {
+                    Button::new(
+                        cx,
+                        |cx| {},
+                        |cx| Icon::new(cx, IconCode::Hierarchy, 24.0, 16.0),
+                    )
+                    .class("top_bar_select");
+                    Button::new(
+                        cx,
+                        |cx| {},
+                        |cx| Icon::new(cx, IconCode::Grid, 24.0, 16.0),
+                    )
+                    .class("top_bar_select");
+                    Button::new(
+                        cx,
+                        |cx| {},
+                        |cx| Icon::new(cx, IconCode::Mixer, 24.0, 16.0),
+                    )
+                    .class("top_bar_select");
+                    Button::new(
+                        cx,
+                        |cx| cx.emit(PanelEvent::TogglePianoRoll),
+                        |cx| Icon::new(cx, IconCode::Piano, 24.0, 16.0),
+                    )
+                    .class("top_bar_select");
+                });
+                HStack::new(cx, |cx| {
+                    Button::new(
+                        cx,
+                        |cx| {},
+                        |cx| Icon::new(cx, IconCode::Automation, 24.0, 16.0),
+                    )
+                    .class("top_bar_select");
+                    Button::new(
+                        cx,
+                        |cx| {},
+                        |cx| Icon::new(cx, IconCode::Sample, 24.0, 16.0),
+                    )
+                    .class("top_bar_select");
+                    Button::new(
+                        cx,
+                        |cx| {},
+                        |cx| Icon::new(cx, IconCode::DrumSequencer, 24.0, 16.0),
+                    )
+                    .class("top_bar_select");
+                    Button::new(
+                        cx,
+                        |cx| {},
+                        |cx| Icon::new(cx, IconCode::Stack, 24.0, 16.0),
+                    )
+                    .class("top_bar_select");
+                });
+            });
+            
+            HStack::new(cx, |cx| {
+                VStack::new(cx, |cx| {
+                    Label::new(cx, "Oscilloscope");
+                    VStack::new(cx, |cx| {
+
+                        Data{input_l: 0.42, input_r: 0.69}.build(cx);
+                        Meter::new(cx, Data::input_l, Direction::LeftToRight)
+                        .class("top_bar_peak");
+                        Meter::new(cx, Data::input_r, Direction::LeftToRight)
+                        .class("top_bar_peak");
+
+                    })
+                    .class("top_bar_peak_container");
+
+                })
+                .class("top_bar_audio_graph_container");
+                
+                VStack::new(cx, |cx| {
+                    Label::new(cx, "Usage Graph")
+                    .top(Stretch(1.0))
+                    .bottom(Stretch(1.0));
+                })
+                .class("top_bar_performance_graph_container");
+                
+            })
+            .class("top_bar_graph_container");
+        })
+        .class("top_bar_right_container");
+        
+        
     })
     .class("top_bar");
 }
--- src/ui/views/meter.rs
+++ src/ui/views/meter.rs
@@ -1,3 +1,4 @@
+use vizia::{vg::femtovg::{Path, Paint, Color}};
 use vizia::*;
 
 /// The direction the meter bar shows the peak in.
@@ -14,19 +15,57 @@ pub enum Direction {
     LeftToRight,
     /// The inverted direction from the standard horizontal meter
     RightToLeft,
+    /// A special round peak meter
     Radial,
 }
 
+#[derive(Debug)]
+pub enum MeterEvents {
+    UpdatePosition(f32),
+    ChangePeakDropSpeed(f32),
+    ChangeSmoothingFactor(f32),
+    ChangeMaxHoldTime(i32),
+    ChangeBarColor(String)
+}
+
 #[derive(Lens)]
-pub struct MeterData {
+pub struct Meter {
     pos: f32,
     max: f32,
     max_delay_ticker: i32,
     max_drop_speed: f32,
+    max_hold_time: i32,
     smoothing_factor: f32,
+    direction: Direction,
+    bar_color: String,
+    line_color: String
 }
 
-impl Model for MeterData {
+impl Meter {
+    pub fn new<L: Lens<Target = f32>>(cx: &mut Context, lens: L, direction: Direction) -> Handle<Self> {
+        vizia::View::build(Self {
+            pos: 0.0,
+            max: 0.0,
+            max_delay_ticker: 0,
+            max_drop_speed: 0.05,
+            max_hold_time: 50,
+            smoothing_factor: 0.1,
+            direction,
+            bar_color: String::from("#ff0000"),
+            line_color: String::from("#000000")
+        }, cx, move |cx| {
+            Binding::new(cx, lens, |cx, value| {
+                cx.emit(MeterEvents::UpdatePosition(value.get(cx)));
+            });
+        })
+    }
+}
+
+impl View for Meter {
+    fn element(&self) -> Option<String> {
+        Some("meter".to_string())
+    }
+
     fn event(&mut self, _cx: &mut Context, event: &mut Event) {
         if let Some(param_change_event) = event.message.downcast() {
             match param_change_event {
@@ -35,7 +74,7 @@ impl Model for MeterData {
 
                     if self.max < self.pos {
                         self.max = self.pos;
-                        self.max_delay_ticker = 50;
+                        self.max_delay_ticker = self.max_hold_time;
                     }
                     if self.max_delay_ticker == 0 {
                         self.max -= self.max_drop_speed;
@@ -53,131 +92,136 @@ impl Model for MeterData {
                 MeterEvents::ChangeSmoothingFactor(n) => {
                     self.smoothing_factor = *n;
                 }
-            }
-        }
-    }
-}
-
-#[derive(Debug)]
-pub enum MeterEvents {
-    UpdatePosition(f32),
-    ChangePeakDropSpeed(f32),
-    ChangeSmoothingFactor(f32),
-}
-
-pub struct Meter<L> {
-    /// The position of the meter in [0,1]
-    lens: L
-}
-
-impl<L: Lens<Target = f32>> Meter<L> {
-    pub fn new(cx: &mut Context, lens: L, direction: Direction) -> Handle<Self> {
-        Self {
-            lens: lens.clone()
-        }
-        .build2(cx, move |cx| {
-            MeterData {
-                pos: 0.0,
-                max: 0.0,
-                max_delay_ticker: 0,
-                max_drop_speed: 0.05,
-                smoothing_factor: 0.1,
-            }
-            .build(cx);
-
-            Binding::new(cx, lens, |cx, value| {
-                cx.emit(MeterEvents::UpdatePosition(value.get(cx)));
-            });
-            ZStack::new(cx, |cx| {
-
-                // Draws the peak meter in the different directions. This currently is set at the initialization and can't be changed
-                match direction {
-                    Direction::DownToUp => {
-                        Element::new(cx)
-                            .height(MeterData::pos.map(|val| Percentage(val * 100.0)))
-                            .top(Stretch(1.0))
-                            .width(Stretch(1.0))
-                            .class("meter_bar");
-
-                        Element::new(cx)
-                            .width(Stretch(1.0))
-                            .height(Pixels(2.0))
-                            .top(Stretch(1.0))
-                            .bottom(MeterData::max.map(|val| Percentage(val * 100.0)))
-                            .class("meter_line");
-                    }
-                    Direction::UpToDown => {
-                        Element::new(cx)
-                            .height(MeterData::pos.map(|val| Percentage(val * 100.0)))
-                            .bottom(Stretch(1.0))
-                            .width(Stretch(1.0))
-                            .class("meter_bar");
-
-                        Element::new(cx)
-                            .width(Stretch(1.0))
-                            .height(Pixels(2.0))
-                            .bottom(Stretch(1.0))
-                            .top(MeterData::max.map(|val| Percentage(val * 100.0)))
-                            .class("meter_line");
-                    }
-                    Direction::LeftToRight => {
-                        Element::new(cx)
-                            .width(MeterData::pos.map(|val| Percentage(val * 100.0)))
-                            .right(Stretch(1.0))
-                            .height(Stretch(1.0))
-                            .class("meter_bar");
-
-                        Element::new(cx)
-                            .height(Stretch(1.0))
-                            .width(Pixels(2.0))
-                            .right(Stretch(1.0))
-                            .left(MeterData::max.map(|val| Percentage(val * 100.0)))
-                            .class("meter_line");
-                    }
-                    Direction::RightToLeft => {
-                        Element::new(cx)
-                            .width(MeterData::pos.map(|val| Percentage(val * 100.0)))
-                            .left(Stretch(1.0))
-                            .height(Stretch(1.0))
-                            .class("meter_bar");
-
-                        Element::new(cx)
-                            .height(Stretch(1.0))
-                            .width(Pixels(2.0))
-                            .left(Stretch(1.0))
-                            .right(MeterData::max.map(|val| Percentage(val * 100.0)))
-                            .class("meter_line");
-                    }
-                    Direction::Radial => {
-                        // This is a circle :)
-                        Element::new(cx)
-                            .top(Stretch(1.0))
-                            .bottom(Stretch(1.0))
-                            .left(Stretch(1.0))
-                            .right(Stretch(1.0))
-                            .height(MeterData::pos.map(|val| Percentage(val * 100.0)))
-                            .width(MeterData::pos.map(|val| Percentage(val * 100.0)))
-                            .border_radius(Percentage(50.0))
-                            .class("meter_bar");
-                    }
+                MeterEvents::ChangeMaxHoldTime(n) => {
+                    self.max_hold_time = *n;
                 }
-            });
-        })
+                MeterEvents::ChangeBarColor(col) => {
+                    self.bar_color = col.clone();
+                }
+                
+            }
+        }
     }
-}
 
-impl<L: Lens<Target = f32>> View for Meter<L> {
-    fn element(&self) -> Option<String> {
-        Some("meter".to_string())
+    fn draw(&self, cx: &mut Context, canvas: &mut Canvas) {
+        let width = cx.cache.get_width(cx.current);
+        let height = cx.cache.get_height(cx.current);
+        let pos_x = cx.cache.get_posx(cx.current);
+        let pos_y = cx.cache.get_posy(cx.current);
+        let value = self.pos;
+        let max = self.max;
+
+        // Create variables for the rectangle
+        let front_x;
+        let front_y;
+        let front_w;
+        let front_h;
+
+        let line_x1;
+        let line_x2;
+        let line_y1;
+        let line_y2;
+
+        // Build the start and end positions of the back and front line
+        // according to the direction the meter is going and the value the meter is showing
+        match self.direction {
+            Direction::DownToUp => {
+                front_x = pos_x;
+                front_y = pos_y + (1.0-value) * height;
+
+                front_w = width;
+                front_h = value * height;
+
+                line_x1 = pos_x;
+                line_x2 = pos_x + width;
+
+                line_y1 = pos_y + (1.0-max) * height;
+                line_y2 = line_y1;
+            },
+            Direction::UpToDown => {
+                front_x = pos_x;
+                front_y = pos_y;
+
+                front_w = width;
+                front_h = value * height;
+
+                line_x1 = pos_x;
+                line_x2 = pos_x + width;
+
+                line_y1 = pos_y + max * height;
+                line_y2 = line_y1;
+            },
+            Direction::LeftToRight => {
+                front_x = pos_x;
+                front_y = pos_y;
+
+                front_w = value * width;
+                front_h = height;
+
+                line_x1 = pos_x + max * width;
+                line_x2 = pos_x + max * width;
+
+                line_y1 = pos_y;
+                line_y2 = pos_y + height;
+            },
+            Direction::RightToLeft => {
+                front_x = pos_x + (1.0-value) * width;
+                front_y = pos_y;
+
+                front_w = value * width;
+                front_h = height;
+
+                line_x1 = pos_x + (1.0 - max) * width;
+                line_x2 = pos_x + (1.0 - max) * width;
+
+                line_y1 = pos_y;
+                line_y2 = pos_y + height;
+            },
+            _ => {
+                front_x = pos_x;
+                front_y = pos_y + (1.0-value) * height;
+
+                front_w = width;
+                front_h = value * height;
+
+                line_x1 = pos_x;
+                line_x2 = pos_x + width;
+
+                line_y1 = pos_y + max * height;
+                line_y2 = line_y1;
+            }
+        };
+
+
+        // Draw the bar
+        if value >= 1e-3 {
+            let mut front_path = Path::new();
+            front_path.rect(front_x, front_y, front_w, front_h);
+
+            let mut front_paint = Paint::color(Color::hex(self.bar_color.as_str()));
+
+            canvas.fill_path(&mut front_path, front_paint);
+
+            let mut line_path = Path::new();
+            line_path.move_to(line_x1, line_y1);
+            line_path.line_to(line_x2, line_y2);
+
+            let mut line_paint = Paint::color(Color::hex(self.line_color.as_str()));
+            line_paint.set_line_width(2.0);
+
+            canvas.stroke_path(&mut line_path, line_paint)
+        }
     }
 }
 
 pub trait MeterHandle {
     fn peak_drop_speed(self, val: impl Res<f32>) -> Self;
     fn smoothing_factor(self, val: impl Res<f32>) -> Self;
+    fn bar_color(self, val: impl Res<String>) -> Self;
+    fn max_hold_time(self, val: impl Res<i32>) -> Self;
 }
 
-impl<T> MeterHandle for Handle<'_, Meter<T>> {
+impl MeterHandle for Handle<'_, Meter> {
     fn peak_drop_speed(self, val: impl Res<f32>) -> Self {
         val.set_or_bind(self.cx, self.entity, |cx, entity, value| {
             entity.emit(cx, MeterEvents::ChangePeakDropSpeed(value));
@@ -189,6 +233,22 @@ impl<T> MeterHandle for Handle<'_, Meter<T>> {
     fn smoothing_factor(self, val: impl Res<f32>) -> Self {
         val.set_or_bind(self.cx, self.entity, |cx, entity, value| {
             entity.emit(cx, MeterEvents::ChangeSmoothingFactor(value));
+        });
+
+        self
+    }
+
+    fn bar_color(self, val: impl Res<String>) -> Self {
+        val.set_or_bind(self.cx, self.entity, |cx, entity, value| {
+            entity.emit(cx, MeterEvents::ChangeBarColor(value));
+        });
+
+        self
+    }
+
+    fn max_hold_time(self, val: impl Res<i32>) -> Self {
+        val.set_or_bind(self.cx, self.entity, |cx, entity, value| {
+            entity.emit(cx, MeterEvents::ChangeMaxHoldTime(value));
         });
 
         self
